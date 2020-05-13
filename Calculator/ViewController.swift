8b2eec03858a6af4cdfc888bb6553b6b552b1f72
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-13 13:50:50 -0400

    Accommodate multiple digits number

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,24 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var displayLabel: UILabel!
+    var isFinishedTypingNumber: Bool = true
     
+    @IBOutlet weak var displayLabel: UILabel!
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle {
-        displayLabel.text = numValue
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
         }
     }
-
+    
 }
 
